55603dddefed1d48d573dd114557003e9765a072
diadestiny <1247889154@qq.com>
2021-05-31 11:50:09 +0800

    完成登录界面layout设计

---
+++ app/src/main/java/com/guet/shareapp/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.guet.shareapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.gc.materialdesign.views.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
