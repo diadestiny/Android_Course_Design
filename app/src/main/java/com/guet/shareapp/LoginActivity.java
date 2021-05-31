69964302c66e8ae7c62e7b455b1df21c43c3de05
diadestiny <1247889154@qq.com>
2021-05-31 12:26:57 +0800

    完善登录模块

--- app/src/main/java/com/guet/shareapp/LoginActivity.java
+++ app/src/main/java/com/guet/shareapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.gc.materialdesign.views.Button;
 
@@ -20,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+
+        TextView registerView = findViewById(R.id.newuser);
+        registerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
     }
